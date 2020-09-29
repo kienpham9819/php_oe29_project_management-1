f4ac404b4023dd330e4a9296aa083a181160bcaf
TDM1911 <trandaomanh1911@gmail.com>
2020-09-29 08:53:04 +0700

    design project migration

---
+++ database/migrations/2020_09_29_022707_create_task_lists_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTaskListsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('task_lists', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->foreignId('project_id');
+            $table->foreignId('user_id');
+            $table->dateTime('due_date');
+            $table->dateTime('start_date')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('task_lists');
+    }
+}
