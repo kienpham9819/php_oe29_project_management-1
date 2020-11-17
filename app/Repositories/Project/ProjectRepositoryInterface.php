c854e76ff1c54eb4ff108812eb3f2839236f08eb
TDM1911 <trandaomanh1911@gmail.com>
2020-11-17 13:03:19 +0700

    implement project repo

---
+++ app/Repositories/Project/ProjectRepositoryInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Repositories\Project;
+
+use App\Repositories\RepositoryInterface;
+
+interface ProjectRepositoryInterface extends RepositoryInterface
+{
+    public function completedTask($id);
+    public function unfinishedTask($id);
+    public function toggle($id);
+    public function projectsFromGroups($groups = [], $paginate = 0);
+}
