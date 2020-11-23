6d8b4b9a2942cb8eb135b17705fd7d6914379313
TDM1911 <trandaomanh1911@gmail.com>
2020-11-23 13:27:49 +0700

    Redesign project view

--- resources/lang/en/project.php
+++ resources/lang/en/project.php
@@ -3,12 +3,17 @@
 return [
     'approved' => 'Approved project',
     'create' => 'Create project',
+    'completed' => 'Project completed',
     'edit' => 'Edit project',
+    'grade' => 'Grade',
+    'link_github' => 'Link Github Repositories',
     'list' => 'Project list',
     'management' => 'Project management',
     'member_list' => 'Member list',
     'pending' => 'Pending project',
     'project' => 'Project',
     'recent' => 'Recent project',
+    'review' => 'Lecturer review',
+    'submit' => 'Submit project',
     'view_members' => 'view members',
 ];
--- resources/lang/vi/project.php
+++ resources/lang/vi/project.php
@@ -3,12 +3,17 @@
 return [
     'approved' => 'Dự án được chấp nhận',
     'create' => 'Tạo dự án mới',
+    'completed' => 'Dự án đã hoàn thành',
     'edit' => 'Sửa dự án',
+    'grade' => 'Điểm số',
+    'link_github' => 'Liên kết Github Repository',
     'list' => 'Danh sách dự án',
     'management' => 'Quản lý dự án',
     'member_list' => 'Danh sách thành viên',
     'pending' => 'Dự án đang chờ duyệt',
     'project' => 'Dự án',
     'recent' => 'Dự án gần đây',
+    'review' => 'Nhận xét của giảng viên',
+    'submit' => 'Nộp dự án',
     'view_members' => 'xem thành viên',
 ];
